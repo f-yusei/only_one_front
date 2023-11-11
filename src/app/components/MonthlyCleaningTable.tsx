c8bb0aacbe6df00199ccdc2a21ecd50df9a2f71a
f-yusei <fkys2932@gmail.com>
2023-11-12 08:46:25 +0900

    "fix: typescriptの型エラーを対処"

--- src/app/components/MonthlyCleaningTable.tsx
+++ src/app/components/MonthlyCleaningTable.tsx
@@ -34,6 +34,7 @@ const monthlyCleaningTable: MonthlyCleaningTableData[] = [
 ];
 
 declare module '@tanstack/table-core' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface TableMeta<TData extends RowData> {
     updateData: (rowIndex: number, columnId: string, value: unknown) => void;
   }
--- src/app/components/WeeklyCleaningTable.tsx
+++ src/app/components/WeeklyCleaningTable.tsx
@@ -24,6 +24,7 @@ import { ActionMeta, MultiValue, Select, GroupBase, OptionBase } from 'chakra-re
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 declare module '@tanstack/table-core' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface TableMeta<TData extends RowData> {
     updateData: (rowIndex: number, columnId: string, value: unknown) => void;
   }
