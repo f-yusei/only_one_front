d4d7da9b74dc0fbed498196e86bf248334018ecf
f-yusei <fkys2932@gmail.com>
2023-11-12 18:22:55 +0900

    "fix: コンポーネントの呼び出し方法を修正"

--- src/app/manage/weeklycleanreport/page.tsx
+++ src/app/manage/weeklycleanreport/page.tsx
@@ -1,66 +1,84 @@
 'use client';
-import { Input,FormControl,FormLabel,FormHelperText,Radio,RadioGroup,Textarea, Select, HStack, Box, Stack } from '@chakra-ui/react';
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Radio,
+  RadioGroup,
+  Textarea,
+  Select,
+  HStack,
+  Box,
+  Stack,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 
-
-
-
 export default function WeeklyCleanReport() {
-    const studentName =  {
-        one:"あーちゃん",
-        two:"いーちゃん",
-        three:"うーちゃん"
-    };
+  const studentName = {
+    one: 'あーちゃん',
+    two: 'いーちゃん',
+    three: 'うーちゃん',
+  };
 
   return (
     <Box m={5}>
-      {AttendChecker(studentName.one)}
-      {AttendChecker(studentName.two)}
-      {WeeklyCleanValue("要項１")} 
-      {WeeklyCleanValue("要項２")} 
-      {WeeklyCleanValue("要項３")} 
-      {WeeklyCleanValue("要項４")} 
-      {WeeklyCleanComment()} 
+      <AttendChecker studentAccount={studentName.one} />
+      <AttendChecker studentAccount={studentName.two} />
+      <WeeklyCleanValue cleanpoint="要項１" />
+      <WeeklyCleanValue cleanpoint="要項２" />
+      <WeeklyCleanValue cleanpoint="要項３" />
+      <WeeklyCleanValue cleanpoint="要項４" />
+      <WeeklyCleanComment />
     </Box>
   );
 }
 
-const AttendChecker = (studentAccount : string) => {
-    const [attendcheck,setAttendCheck] = useState('出席');
+type AttendCheckerProps = {
+  studentAccount: string;
+};
+
+const AttendChecker = ({ studentAccount }: AttendCheckerProps) => {
+  const [attendcheck, setAttendCheck] = useState('出席');
   return (
     <>
-    <Box>
-    <HStack>
-      <Box width="80%">{studentAccount}</Box>
-      <Select
-      onChange={(e) => {
-       setAttendCheck(e.target.value)
-      }} 
-      placeholder="選択してください">
-        <option value={studentAccount + "attend"}>出席</option>
-        <option value={studentAccount + "absence"}>欠席</option>
-        <option value={studentAccount + "substitute"}>代理</option>
-      </Select>
-    </HStack>
-    </Box>
-    { attendcheck === studentAccount + "substitute" ? Substitute() :""}
-</>
+      <Box>
+        <HStack>
+          <Box width="80%">{studentAccount}</Box>
+          <Select
+            onChange={(e) => {
+              setAttendCheck(e.target.value);
+            }}
+            placeholder="選択してください"
+          >
+            <option value={studentAccount + 'attend'}>出席</option>
+            <option value={studentAccount + 'absence'}>欠席</option>
+            <option value={studentAccount + 'substitute'}>代理</option>
+          </Select>
+        </HStack>
+      </Box>
+      {attendcheck === studentAccount + 'substitute' ? Substitute() : ''}
+    </>
   );
 };
 
-const WeeklyCleanValue = (cleanpoint: string) => {
+type WeeklyCleanValueProps = {
+  cleanpoint: string;
+};
+
+const WeeklyCleanValue = ({ cleanpoint }: WeeklyCleanValueProps) => {
   return (
     <Box>
       <Box>{cleanpoint}</Box>
-     <RadioGroup >
-        <Stack direction='row'>
-            <Radio value='1'>1</Radio>
-            <Radio value='2'>2</Radio>
-            <Radio value='3'>3</Radio>
-            <Radio value='4'>4</Radio>
-            <Radio value='5'>5</Radio>
+      <RadioGroup>
+        <Stack direction="row">
+          <Radio value="1">1</Radio>
+          <Radio value="2">2</Radio>
+          <Radio value="3">3</Radio>
+          <Radio value="4">4</Radio>
+          <Radio value="5">5</Radio>
         </Stack>
-     </RadioGroup>
+      </RadioGroup>
     </Box>
   );
 };
@@ -74,11 +92,11 @@ const WeeklyCleanComment = () => {
 };
 
 const Substitute = () => {
-    return (
-       <FormControl>
-        <FormLabel>代理者を入力してください</FormLabel>
-        <Input type = 'text' />
-        <FormHelperText>例：1M 高専太郎</FormHelperText>
-       </FormControl>
-    );
+  return (
+    <FormControl>
+      <FormLabel>代理者を入力してください</FormLabel>
+      <Input type="text" />
+      <FormHelperText>例：1M 高専太郎</FormHelperText>
+    </FormControl>
+  );
 };
