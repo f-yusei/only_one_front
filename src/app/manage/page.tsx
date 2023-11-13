db02c93fb5cc47fd9c804ce2c47ca64e6a81f894
f-yusei <fkys2932@gmail.com>
2023-11-13 14:16:59 +0900

    "feat: 月と寮棟を入力していない時に表を作成できないように実装"

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -3,17 +3,36 @@ import { Box, Button, HStack, Text } from '@chakra-ui/react';
 import WeeklyCleaningTable from '../components/WeeklyCleaningTable';
 import MonthlyCleaningTable from '../components/MonthlyCleaningTable';
 import PreviewModal from '../components/PreviewModal';
+import { useEffect, useState } from 'react';
+import SelectMonthAndDormitory from '../components/common';
 
 export default function Home() {
+  const [dormitory, setDormitory] = useState('');
+  const [month, setMonth] = useState('');
+  const [isEditMode, setIsEditMode] = useState(false);
+  useEffect(() => {
+    if (month === '' || dormitory === '') {
+      setIsEditMode(false);
+      return;
+    }
+    setIsEditMode(true);
+  }, [month, dormitory]);
   return (
     <Box p={4}>
+      <SelectMonthAndDormitory
+        dormitory={dormitory}
+        month={month}
+        setDormitory={setDormitory}
+        setMonth={setMonth}
+        tableName="清掃当番表"
+      />
       <Box>
         <Text>週例清掃</Text>
-        <WeeklyCleaningTable isEditMode={true} />
+        <WeeklyCleaningTable isEditMode={isEditMode} />
       </Box>
       <Box>
         <Text>月例清掃</Text>
-        <MonthlyCleaningTable isEditMode={true} />
+        <MonthlyCleaningTable isEditMode={isEditMode} />
       </Box>
       <HStack>
         <PreviewModal />
