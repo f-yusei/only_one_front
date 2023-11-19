6b7bd6134ea857f83c2f6d2e067fa33d518b4b3b
f-yusei <fkys2932@gmail.com>
2023-11-19 22:02:29 +0900

    "feat: 環境変数が設定されていなかったときのエラーを作成"

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
+if (!baseURL) {
+  throw new Error('NEXT_PUBLIC_BACKEND_API_URL is not defined');
+}
+
 const apiClient = axios.create({
   withCredentials: true,
   baseURL: baseURL + '/api',
